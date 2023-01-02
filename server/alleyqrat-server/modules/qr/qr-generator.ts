e7d81329c871f46527e0f8515a020eba252b5c17
Franco Canevali <francocanevali@gmail.com>
2023-01-02 11:57:44 -0300

    Removed console.log

--- server/alleyqrat-server/modules/qr/qr-generator.ts
+++ server/alleyqrat-server/modules/qr/qr-generator.ts
@@ -10,7 +10,6 @@ export default class QRGenerator {
   public async generateQR(): Promise<IQR> {
     return new Promise<IQR>(async(resolve, reject)=>{
       const base64 = await QRCode.toDataURL('ASD');
-      console.log(base64);
       const qr:IQR = {code: {base64}}
       resolve(qr);
     });
