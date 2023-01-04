3f2c0d5d91a37167fd45f2407957730ba3570731
Franco Canevali <francocanevali@gmail.com>
2023-01-04 20:48:34 -0300

    Decodes and next steps

--- server/alleyqrat-server/src/__tests__/qr/encoder.ts
+++ server/alleyqrat-server/src/__tests__/qr/encoder.ts
@@ -3,9 +3,22 @@ import '@testing-library/jest-dom'
 
 describe('QR Encoder', ()=>{
   test('Encodes data into a data:image/png;base64 string of a QR Code', async () => {
-    const qrCode = await QR.generate("asd");
+    const qrCode = await QR.generate("messlife");
     const b64dataImgHeader = 'data:image/png;base64';
     expect(qrCode).toHaveProperty("code.base64");
     expect(qrCode.code.base64.includes(b64dataImgHeader)).toBeTruthy();
+  });
+
+  test('Next steps', async () => {
+    `
+      Client that reads QR
+      Server that generates QR with an URI like /checkpoint/arrived/{qr_checkpoint_identifier}
+      Client scans QR:
+        Two API Calls:
+          - /verify/im/at/checkpoint/{qr_checkpoint_identifier}?loc=lat,lon
+              => (verifies location?) => Persists (userid, has_scanned_qr_uuid)
+              => /checkpoint/arrived/{qr_checkpoint_identifier}?verif=has_scanned_qr_uuid
+    `;
+    expect(false).toBe(true);
   });
 });
--- server/alleyqrat-server/src/modules/qr/decoder.ts
+++ server/alleyqrat-server/src/modules/qr/decoder.ts
@@ -1,20 +1,21 @@
 import { IQRCode } from './qr';
-import QrcodeDecoder from 'qrcode-decoder';
-var qrDecoder = new QrcodeDecoder();
+import jsQR from 'jsqr';
+import { PNG } from 'pngjs';
 
 export default class QRDecoder {
-  public async decode(qr: IQRCode) {
+  public async decode(qr: IQRCode): Promise<string|undefined> {
     return new Promise((resolve, reject) => {
-      resolve("lol");
-      qrDecoder
-      .decodeFromImage(qr.code.base64)
-      .then((decoded) => {
-        console.log("HAVE DECODED: ", decoded); 
-        resolve(decoded);
-      })
-      .catch((err) => {
-        reject(err);
-      });
+      const image = this.base64toPNG(qr.code.base64);
+      const code = jsQR(Uint8ClampedArray.from(image.data), image.width, image.height);
+      resolve(code?.data);
     });
+  }
+
+  private base64toPNG(base64: string): PNG {
+    return PNG.sync.read(this.base64toBuffer(base64));
+  }
+
+  private base64toBuffer(base64: string): Buffer {
+      return Buffer.from(base64.slice('data:image/png;base64'.length), 'base64')
   }
 }
