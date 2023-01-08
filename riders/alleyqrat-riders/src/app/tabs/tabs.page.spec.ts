640cf577776a292a8523b9aabfde1ea4c95ea595
Franco Canevali <francocanevali@gmail.com>
2023-01-08 19:11:47 -0300

    Test for nav bar component

--- riders/alleyqrat-riders/src/app/tabs/tabs.page.spec.ts
+++ riders/alleyqrat-riders/src/app/tabs/tabs.page.spec.ts
@@ -1,5 +1,6 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {NavigationEnd} from '@angular/router';
 
 import { TabsPage } from './tabs.page';
 
@@ -23,4 +24,53 @@ describe('TabsPage', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have an ion-tab-bar element', () => {
+    const tabBar = fixture.nativeElement.querySelector('ion-tab-bar');
+    expect(tabBar).toBeDefined();
+  });
+
+  it('should have the ion-tab-bar at the bottom', () =>{
+    const tabBar = fixture.nativeElement.querySelector('ion-tab-bar') as HTMLIonTabBarElement;
+    const tabPosition = tabBar.getAttribute('slot');
+    expect(tabPosition).toBeDefined();
+    expect(tabPosition).toBe('bottom');
+  });
+
+  it('should have the ion-tab-bar with the same number of children as tabs defined', () =>{
+    const tabBar = fixture.nativeElement.querySelector('ion-tab-bar');
+    const tabChildCount = (tabBar.childElementCount as number);
+    expect(tabChildCount).toBe(component.tabs.length);
+  });
+
+  it('should have all ion-tab-button with icons and labels only', () =>{
+    const tabButtons = (fixture.nativeElement.querySelectorAll('ion-tab-button') as Array<HTMLIonTabButtonElement>);
+    tabButtons.forEach((button) => {
+      const icon = button.childNodes[0] as HTMLElement;
+      const label = button.childNodes[1] as HTMLElement;
+      expect(button.childElementCount).toBe(2);
+      expect(icon.tagName.toLowerCase()).toBe('ion-icon');
+      expect(label.tagName.toLowerCase()).toBe('ion-label');
+    });
+  });
+
+  it('should set active icon only for active tab', () => {
+    const navigationEvent = new NavigationEnd(0, '/', '/home');
+    component.changeIconForActiveTab(navigationEvent);
+    fixture.detectChanges();
+    const icons = Array.from(fixture.nativeElement.querySelectorAll('ion-icon') as NodeList) as Array<HTMLIonIconElement>;
+    let inactiveIcons = icons.filter((icon: HTMLIonIconElement) => icon.name?.toString().indexOf('-outline') !== -1).length;
+    let activeIcon = icons.filter((icon: HTMLIonIconElement) => icon.name?.toString().indexOf('-outline') == -1).length;
+    expect(inactiveIcons).toBe(component.tabs.length - 1);
+    expect(activeIcon).toBe(1);
+
+    const navigationEvent2 = new NavigationEnd(0, '/map', '');
+    component.changeIconForActiveTab(navigationEvent2);
+    fixture.detectChanges();
+    inactiveIcons = icons.filter((icon: HTMLIonIconElement) => icon.name?.toString().indexOf('-outline') !== -1).length;
+    activeIcon = icons.filter((icon: HTMLIonIconElement) => icon.name?.toString().indexOf('-outline') == -1).length;
+    expect(inactiveIcons).toBe(component.tabs.length - 1);
+    expect(activeIcon).toBe(1);
+  });
+
 });
--- riders/alleyqrat-riders/src/app/tabs/tabs.page.ts
+++ riders/alleyqrat-riders/src/app/tabs/tabs.page.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { Event, NavigationEnd, Router } from '@angular/router';
+
+interface ITab {
+  tabName: string,
+  title: string,
+  icon: string
+}
 
 @Component({
   selector: 'app-tabs',
@@ -6,7 +13,63 @@ import { Component } from '@angular/core';
   styleUrls: ['tabs.page.scss']
 })
 export class TabsPage {
+  public tabs: Array<ITab> = [
+    {
+      tabName: 'home',
+      title: 'Home',
+      icon: 'home-outline'
+    },
+    {
+      tabName: 'map',
+      title: 'Map',
+      icon: 'map-outline'
+    },
+    {
+      tabName: 'scan',
+      title: 'Scan',
+      icon: 'qr-code-outline',
+    },
+    {
+      tabName: 'settings',
+      title: 'Settings',
+      icon: 'settings-outline'
+    }
+  ];
 
-  constructor() {}
+  constructor(private router: Router) {
+    this.router.events.subscribe({
+      next: this.changeIconForActiveTab.bind(this)
+    });
+  }
 
+  public changeIconForActiveTab(event: Event){
+    const isUrlReady = event instanceof NavigationEnd;
+    if(!isUrlReady) return;
+    const tab = this.getTabFromUrl(event.urlAfterRedirects || event.url);
+    this.markTabAsActive(tab);
+    this.markTabsAsInactiveExcept(tab);
+  }
+
+  private markTabsAsInactiveExcept(tab: ITab): void {
+    const indexOfCurrentTab = this.tabs.indexOf(tab);
+    this.tabs.map((tab, index) => {
+      const isAlreadyInactive = tab.icon.indexOf('-outline') == -1;
+      const isNotCurrentTab = index != indexOfCurrentTab;
+      if(isAlreadyInactive && isNotCurrentTab)
+        tab.icon = `${tab.icon}-outline`;
+    })
+  }
+
+  private markTabAsActive(tab: ITab): void {
+    const tabIndex = this.tabs.indexOf(tab);
+    const newIcon = this.tabs[tabIndex].icon.replace('-outline', ''); 
+    this.tabs[tabIndex].icon = newIcon; 
+  }
+
+
+  private getTabFromUrl(url: string): ITab {
+    const tabName = (url.split('/').pop() as string);
+    const tab = (this.tabs.filter(tab => tab.tabName == tabName).pop() as ITab);
+    return tab;
+  }
 }
